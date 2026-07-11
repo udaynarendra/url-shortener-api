9b38f83f3e778c88f0ab76607a56279ab14f5c9f
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-07-11 19:09:56 +0530

    add registerValidation using joi

---
+++ src/validators/validations.js
@@ -0,0 +1,28 @@
+import Joi from 'joi';
+const urlValidation=Joi.object({
+    url:Joi.string()
+    .trim()
+    .url()
+    .required()
+})
+const registerValidation=Joi.object({
+    name:Joi.string()
+    .trim()
+    .min(3)
+    .max(20)
+    .required(),
+    email:Joi.string()
+    .trim()
+    .lowercase()
+    .email()
+    .required(),
+    password:Joi.string()
+    .trim()
+    .min(8)
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^#])[A-Za-z\d@$!%*?&^#]{8,}$/)
+    .required()
+    .messages({
+        "string.pattern.base":"Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character."
+    })
+})
+export {urlValidation};
