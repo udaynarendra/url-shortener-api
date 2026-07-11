974cca43101ac9055c31cb2657fbe8a6706348ff
udaynarendra <udaynarendragudiguntla@gmail.com>
2026-07-11 19:27:46 +0530

    add loginvalidation using joi

--- src/validators/validations.js
+++ src/validators/validations.js
@@ -25,4 +25,19 @@ const registerValidation=Joi.object({
         "string.pattern.base":"Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character."
     })
 })
-export {urlValidation};
+const loginValidation=Joi.object({
+    email:Joi.string()
+    .trim()
+    .lowercase()
+    .email()
+    .required(),
+    password:Joi.string()
+    .trim()
+    .min(8)
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^#])[A-Za-z\d@$!%*?&^#]{8,}$/)
+    .required()
+    .messages({
+        "string.pattern.base":"Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character"
+    })
+})
+export {urlValidation,registerValidation,loginValidation};
